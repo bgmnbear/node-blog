2f7af1254d8c7f7b67f2316693174c14981c80a2
Jason Whister <jasonwhister@gmail.com>
2018-09-07 16:34:23 +0800

    Add middleware

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const path = require('path')
 const express = require('express')
 const app = express()
-const indexRouter = require('./routes/index')
-const userRouter = require('./routes/users')
 
-app.set('views', path.join(__dirname, 'views'))// 设置存放模板文件的目录
-app.set('view engine', 'ejs')// 设置模板引擎为 ejs
+app.use(function (req, res, next) {
+  console.log('1')
+  next(new Error('haha'))
+})
 
-app.use('/', indexRouter)
-app.use('/users', userRouter)
+app.use(function (req, res, next) {
+  console.log('2')
+  res.status(200).end()
+})
+
+//错误处理
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 app.listen(3000)
